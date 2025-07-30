bb7a8349d653028b9060cb161ead51d2fa68c427
wwwalohacampus <h850415@naver.com>
2025-07-30 17:45:15 +0900

    123

--- Board/board-app/src/components/board/Update.jsx
+++ Board/board-app/src/components/board/Update.jsx
@@ -1,8 +1,49 @@
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import styles from './css/Update.module.css'
+import { useEffect, useState } from 'react'
 
 
-const Update = () => {
+const Update = ({ board, onUpdate, onDelete }) => {
+
+  // 🧊 state
+  const [title, setTitle] = useState('')
+  const [writer, setWriter] = useState('')
+  const [content, setContent] = useState('')
+
+  // 변경 이벤트 함수
+  const changeTitle = (e) => { setTitle( e.target.value ) }
+  const changeWriter = (e) => { setWriter( e.target.value ) }
+  const changeContent = (e) => { setContent( e.target.value ) }
+
+  const { id } = useParams()
+
+  // 수정 함수
+  const onSubmit = () => {
+    const data = {
+      'id' : id,
+      'title': title,
+      'writer': writer,
+      'content': content
+    }
+    const headers = { 'Content-Type' : 'application/json' }
+
+    onUpdate(data, headers)
+  }
+
+  useEffect(() => {
+    if( board ) {
+      setTitle(board.title)
+      setWriter(board.writer)
+      setContent(board.content)
+    }
+  }, [board])
+
+  const handleDelete = () => {
+    const check = window.confirm('정말로 삭제하시겠습니까?')
+    if( check )
+      onDelete(id)
+  }
+
   return (
     <div className="container">
       <h1 className="title">게시글 수정</h1>
@@ -10,18 +51,20 @@ const Update = () => {
         <tr>
           <th>제목</th>
           <td>
-            <input type="text" className={styles['form-input']}/>
+            <input type="text" onChange={changeTitle} value={title} className={styles['form-input']}/>
           </td>
         </tr>
         <tr>
           <th>작성자</th>
           <td>
-            <input type="text" className={styles['form-input']} />
+            <input type="text" onChange={changeWriter} value={writer} className={styles['form-input']} />
           </td>
         </tr>
         <tr>
           <td colSpan={2}>
             <textarea cols={40} rows={10} 
+              value={content}
+              onChange={changeContent}
               className={styles['form-input']}></textarea>
           </td>
         </tr>
@@ -31,8 +74,8 @@ const Update = () => {
           <Link to="/boards" className="btn">목록</Link>
         </div>
         <div>
-          <button className="btn">수정</button>
-          <button className="btn">삭제</button>
+          <button className="btn" onClick={onSubmit}>수정</button>
+          <button className="btn" onClick={handleDelete}>삭제</button>
         </div>
       </div>
     </div>
